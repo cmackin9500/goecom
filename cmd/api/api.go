0b232a66a73ee00afaeef05d084321ccdfa4368b
cmackin9500 <casey@mackinnon.org>
2024-09-20 15:23:01 +0400

    Initial commit

---
+++ cmd/api/api.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	"github.com/cmackin9500/goecom/service/user"
+	"github.com/gorilla/mux"
+)
+
+type APIServer struct {
+	address string
+	db *sql.DB
+}
+
+func NewAPIServer(address string, db *sql.DB) *APIServer {
+	return &APIServer{
+		address: address,
+		db: db,
+	}
+}
+
+func (s *APIServer) Run() error {
+	router := mux.NewRouter()
+	subrouter := router.PathPrefix("api/v1").Subrouter()
+
+	userStore := user.NewStore(s.db)
+	userHandler := user.NewHandler(userStore)
+	userHandler.RegisterRoutes(subrouter)
+
+	log.Println("Listening on", s.address)
+	return http.ListenAndServe(s.address, router)
+}
---
+++ cmd/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/cmackin9500/goecom/cmd/api"
+	"github.com/cmackin9500/goecom/config"
+	"github.com/cmackin9500/goecom/db"
+	"github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User: config.Envs.DBUser,
+		Passwd: config.Envs.DBPassword,
+		Addr: config.Envs.DBAddress,
+		DBName: config.Envs.DBName,
+		Net: "tcp",
+		AllowNativePasswords: true,
+		ParseTime: true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initStorage(db)
+
+	server := api.NewAPIServer((":8080"), db)
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(("DB: Successfully connected."))
+}
